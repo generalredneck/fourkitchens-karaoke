79298c07da5f3437ae7cd4b78d22efa74c9d8fe4
Allan Chappell <generalredneck@gmail.com>
2019-02-05 15:15:51 -0600

    using the unsafe-perm flag because some people may be using root to manage this project

--- scripts/composer/prepare-theme.sh
+++ scripts/composer/prepare-theme.sh
@@ -2,5 +2,5 @@
 
 THEMEPATH=$1
 cd "$THEMEPATH"
-[ -f pattern-lab/composer.json ] && echo "Node libraries pulled from cache. Skipping..." || npm install
+[ -f pattern-lab/composer.json ] && echo "Node libraries pulled from cache. Skipping..." || npm install --unsafe-perm
 ./node_modules/.bin/gulp build
