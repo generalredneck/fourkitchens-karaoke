850350e0779d95a8ec04940790b56caeba7e9b9e
Gregg Hendrix <gregg@fourkitchens.com>
2022-09-02 14:08:28 -0700

    feat: displaying the object semantically (not finished yet)

--- web/themes/custom/kitchen_karaoke/components/02-molecules/recent-requests/recent-requests.stories.js
+++ web/themes/custom/kitchen_karaoke/components/02-molecules/recent-requests/recent-requests.stories.js
@@ -5,6 +5,33 @@ import recentRequestData from './recent-requests.yml';
 /**
  * Storybook Definition.
  */
-export default { title: 'Molecules/Recent Requests' };
+export default {
+  title: 'Molecules/Recent Requests',
+  parameters: {
+    layout: 'fullscreen',
+  },
+  argTypes: {
+    title_prefix: {
+      name: 'Title Prefix',
+      type: 'string',
+      defaultValue: recentRequestData.title_prefix,
+    },
+    label: {
+      name: 'Label',
+      type: 'string',
+      defaultValue: recentRequestData.label,
+    },
+    content: {
+      name: 'Recent Requests Content',
+      type: 'string',
+      defaultValue: recentRequestData.content,
+    },
+    title_suffix: {
+      name: 'Title Suffix',
+      type: 'string',
+      defaultValue: recentRequestData.title_suffix,
+    },
+  },
+};
 
 export const recentRequests = () => recentRequestTwig(recentRequestData);
