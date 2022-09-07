2f34ccc40a59fd1c559ef342bf185d6002f65ccb
Gregg Hendrix <gregg@fourkitchens.com>
2022-09-07 13:01:51 -0700

    feat: disply label, title, and prefix in storybook

--- web/themes/custom/kitchen_karaoke/components/02-molecules/recent-requests/recent-requests.stories.js
+++ web/themes/custom/kitchen_karaoke/components/02-molecules/recent-requests/recent-requests.stories.js
@@ -5,33 +5,6 @@ import recentRequestData from './recent-requests.yml';
 /**
  * Storybook Definition.
  */
-export default {
-  title: 'Molecules/Recent Requests',
-  parameters: {
-    layout: 'fullscreen',
-  },
-  argTypes: {
-    title_prefix: {
-      name: 'Title Prefix',
-      type: 'string',
-      defaultValue: recentRequestData.title_prefix,
-    },
-    label: {
-      name: 'Label',
-      type: 'string',
-      defaultValue: recentRequestData.label,
-    },
-    content: {
-      name: 'Recent Requests Content',
-      type: 'string',
-      defaultValue: recentRequestData.content,
-    },
-    title_suffix: {
-      name: 'Title Suffix',
-      type: 'string',
-      defaultValue: recentRequestData.title_suffix,
-    },
-  },
-};
+export default { title: 'Molecules/Recent Requests' };
 
 export const recentRequests = () => recentRequestTwig(recentRequestData);
