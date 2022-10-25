072d880b5df9800eb1ab61598385f6615df99a08
Uri Frazier <uri@fourkitchens.com>
2022-10-25 06:42:21 -0700

    Fixes some PHPCS space/indentation syntax stuff

--- web/modules/custom/fk_glossary_block/src/Plugin/Block/GlossaryLinksBlock.php
+++ web/modules/custom/fk_glossary_block/src/Plugin/Block/GlossaryLinksBlock.php
@@ -14,40 +14,44 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("Custom")
  * )
  */
-class GlossaryLinksBlock extends BlockBase {
+class GlossaryLinksBlock extends BlockBase
+{
 
-  /**
-   * {@inheritdoc}
-   */
-  public function build() {
-      $route_name = \Drupal::routeMatch()->getRouteName();
-      $chars = array_merge(range('A', 'Z'), ['0-9', '#']);
-      foreach($chars as $char) {
-        $items[] = Link::createFromRoute($char, $route_name, [
-          'starts-with' => $char,
-        ]);
-      }
+    /**
+     * {@inheritdoc}
+     */
+    public function build()
+    {
+        $route_name = \Drupal::routeMatch()->getRouteName();
+        $chars = array_merge(range('A', 'Z'), ['0-9', '#']);
+        foreach($chars as $char) {
+            $items[] = Link::createFromRoute(
+                $char, $route_name, [
+                'starts-with' => $char,
+                ]
+            );
+        }
 
 
-    $build = [
-      '#theme' => 'item_list',
-      '#items' => $items,
-      '#cache' => [
+        $build = [
+        '#theme' => 'item_list',
+        '#items' => $items,
+        '#cache' => [
         'contexts' => [
           'url.path',
           'url.query_args',
         ],
-      ],
-      '#attributes' => [
+        ],
+        '#attributes' => [
         'class' => [],
-      ],
-      '#attached' => [
+        ],
+        '#attached' => [
         'library' => [
           'fk_glossary_block/drupal.fk_glossary_block.facet_css',
         ],
-      ]
-    ];
-    return $build;
-  }
+        ]
+        ];
+        return $build;
+    }
 
 }
